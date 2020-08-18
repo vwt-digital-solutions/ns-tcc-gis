7f61c7652d936ddb9cdb257ea89f4df889cd945f
JelleBwns <jellebouwens@hotmail.com>
2020-08-18 16:40:32 +0200

    DAT-3971 Add function to edit features and start of event handling

--- functions/send_hosts/main.py
+++ functions/send_hosts/main.py
@@ -42,10 +42,23 @@ def add_feature(x, y, attributes, layer):
     return res["addResults"][0]["objectId"]
 
 
+def update_feature(x, y, attributes, layer):
+    updates = [
+        {
+            "geometry": {"x": x, "y": y},
+            "attributes": attributes
+        }
+    ]
+
+    res = arcgis_feature("updates", updates, layer)
+
+    return res["updateResults"]
+
+
 def delete_feature(data, layer):
     res = arcgis_feature("deletes", data, layer)
 
-    return res
+    return res["deleteResults"]
 
 
 def arcgis_feature(function, data, layer):
@@ -61,7 +74,7 @@ def arcgis_feature(function, data, layer):
 def do_host(data):
     for host in data["ns_tcc_hosts"]:
         # Check if host is already posted on ArcGIS
-        ref = db.collection(u'hosts').document(host["id"])
+        ref = db.collection("hosts").document(host["id"])
         doc = ref.get()
 
         if not doc.exists:
@@ -83,36 +96,132 @@ def do_host(data):
                     config.LAYER["hosts"]
                 )
             except (TypeError, ValueError) as e:
-                logging.error(f'Error when adding feature: {e}')
-                logging.info(f'Message: {host}')
+                logging.error(f"Error when adding feature: {e}")
+                logging.info(f"Message: {host}")
                 continue
 
-            logging.info(f'Successfully added {host["id"]} as feature with object_id {object_id}')
+            logging.info(f"Successfully added '{host['id']}' as feature with object_id: {object_id}")
 
             ref.set({
-                u'object_id': object_id,
+                "object_id": object_id,
+                "siteName": host["siteName"],
+                "hostGroups": host["hostGroups"],
+                "bssGlobalCoverage": host["bssGlobalCoverage"],
+                "bssHwFamily": host["bssHwFamily"],
+                "bssLifecycleStatus": host["bssLifecycleStatus"],
+                "longitude": host["longitude"],
+                "latitude": host["latitude"]
             })
         else:
-            logging.info(f'Feature with id {host["id"]} was already added')
+            # Document exists so check if info from document and host data is the same
+            doc_info = doc.to_dict()
+            keys = ["hostGroups", "bssGlobalCoverage", "bssHwFamily", "bssLifecycleStatus"]
+
+            doc_info_parsed = {k: doc_info[k] for k in keys}
+            host_parsed = {k: host[k] for k in keys}
+
+            if doc_info_parsed == host_parsed:
+                # Both items are the same thus the feature doesn't have to be updated.
+                logging.info(f"Feature with id {host['id']} was already added")
+            else:
+                # The data is not the same so the feature has to be updated.
+                attributes = {}
+                for key in keys:
+                    if doc_info[key] != host[key]:
+                        attributes[key] = host[key]
+
+                logging.info(f"Changed info is: {attributes}")
+
+                ref.update(
+                    attributes
+                )
+
+                arcgis_updates = {
+                    "objectid": doc_info["object_id"],
+                    "host_groups": host["hostGroups"],
+                    "globalcoverage": host["bssGlobalCoverage"],
+                    "hwfamily": host["bssHwFamily"],
+                    "lifecyclestatus": host["bssLifecycleStatus"]
+                }
+                res = update_feature(doc_info["longitude"], doc_info["latitude"], arcgis_updates, config.LAYER["hosts"])
+                logging.info(res)
+
+                if res[0]["success"]:
+                    logging.info(f"Succesfully updated feature with object_id: {doc_info['object_id']}")
+                else:
+                    logging.info(f"Failed to update feature with object_id: {doc_info['object_id']}")
+
+
+def do_event(data):
+    for event in data["ns_tcc_events"]:
+        # Make unique identifier
+        unique_id = event["siteName"] + "_" + event["hostName"]
+
+        host_ref = db.collection("hosts").document(unique_id)
+        host_doc = host_ref.get()
+
+        if not host_doc.exists:
+            logging.error(f"Trying to make event feature but no host info found with id: {unique_id}")
+            continue
+
+        host_info = host_doc.to_dict()
+
+        # Check if there's already a feature of this event on ArcGIS (firestore)
+        event_ref = db.collection("events").document(unique_id)
+        event_doc = event_ref.get()
+
+        if event_doc.exists:
+            # There is already a feature. Check if statetype changed and delete and recreate feature if needed.
+            pass
+        else:
+            # There is no feature yet so create new feature and add information to firestore
+            attributes = {
+                "id": event["id"],
+                "sitename": event["siteName"],
+                "type": event["type"],
+                "hostname": event["hostName"],
+                "servicedescription": event["serviceDescription"],
+                "statetype": event["stateType"],
+                "output": event["output"],
+                "longounewtput": event["longOutput"],
+                "eventstate": event["eventState"],
+                "timestamp": event["timestamp"]
+            }
+
+            object_id = add_feature(
+                host_info["longitude"],
+                host_info["latitude"],
+                attributes,
+                config.LAYER[event["stateType"]]
+            )
+
+            event_ref.set({
+                "event_id": event["id"],
+                "object_id": object_id,
+                "layer": config.LAYER[event["stateType"]],
+                "stateType": event["stateType"]
+            })
+
+    return
 
 
 def main(request):
     try:
-        envelope = json.loads(request.data.decode('utf-8'))
-        bytes = base64.b64decode(envelope['message']['data'])
+        envelope = json.loads(request.data.decode("utf-8"))
+        bytes = base64.b64decode(envelope["message"]["data"])
         data = json.loads(bytes)
-        subscription = envelope['subscription'].split('/')[-1]
+        subscription = envelope["subscription"].split('/')[-1]
 
-        logging.info(f'Read message from subscription {subscription}')
+        logging.info(f"Read message from subscription {subscription}")
     except Exception as e:
-        logging.error(f'Extracting of data failed: {e}')
-        return 'Error', 500
+        logging.error(f"Extracting of data failed: {e}")
+        return "Error", 500
 
     if subscription in config.SUBS["host"]:
         do_host(data)
     elif subscription in config.SUBS["host"]:
-        pass
+        do_event(data)
     else:
-        logging.info(f'Invalid subscription received: {subscription}')
+        logging.info(f"Invalid subscription received: {subscription}")
 
     return 'OK', 204
